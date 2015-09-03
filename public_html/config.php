7bb68861c1c8c8a64eb9821ceb070bbdcd03a7c8
Jamie Wickham-Jones <jamie@libertytech.com>
2015-09-03 15:48:06 +0100

    dlfkggit add .

--- public_html/config.php
+++ public_html/config.php
@@ -5,7 +5,7 @@ define('HASH_KEY', 'wanker');
 define('URL','http://178.62.67.190/tryagain/nipple/public_html/');
 
 define('DB_TYPE','mysql');
-define('DB_HOST','localhost');
+define('DB_HOST','127.0.0.1');
 define('DB_NAME','proj1');
 define('DB_USER','root');
 define('DB_PASS','test');
