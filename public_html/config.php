3bff3a2cd6cd719d921da95d0791edb33639bfff
Jamie Wickham-Jones <jamie@libertytech.com>
2015-09-03 15:17:11 +0100

    sorted config a bit

--- public_html/config.php
+++ public_html/config.php
@@ -1,15 +1,13 @@
 <?php  
-include ROOT_D.'../settings.php';
-
 
 define('HASH_KEY', 'wanker');
 
-define('URL','http://178.62.67.190/testmvc/public_html/');
+define('URL','http://178.62.67.190/tryagain/nipple/public_html/');
 
 define('DB_TYPE','mysql');
 define('DB_HOST','fatman');
-define('DB_NAME',$info['db_name']);
-define('DB_USER',$info['db_user']);
-define('DB_PASS',$info['db_pass']);
+define('DB_NAME','proj1');
+define('DB_USER','root');
+define('DB_PASS','test');
 
 /** encrypted password for 'test' is: 009fe36c4b136263b5c64f46c13a16d4 */
