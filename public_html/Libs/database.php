ed00ececf6faba6ee8b44dfb46aae6e2c7d17f28
Jamie Wickham-Jones <jamie@libertytech.com>
2015-09-03 15:46:37 +0100

    zsdfjn

--- public_html/Libs/database.php
+++ public_html/Libs/database.php
@@ -4,9 +4,12 @@
 
 class database extends PDO {
 
-	function __construct(){
+	public function __construct(){
 
-		parent::__construct(DB_TYPE.':host= '.DB_HOST.';dbname='.DB_NAME, DB_USER ,DB_PASS);
+			parent::__construct(DB_TYPE.':host= '.DB_HOST.';dbname='.DB_NAME, DB_USER ,DB_PASS);
+
+
+
 
 	}
 
--- public_html/Models/index_model.php
+++ public_html/Models/index_model.php
@@ -5,10 +5,7 @@ class index_model extends model {
 
 	public function __construct()
 	{
-		echo 1;
-		//$stmt = $this->db->prepare
-
-		
+	
 	}
 
 	
