12a8b978d76b5252aaa530650c64d53811b4962d
Jamie Wickham-Jones <jamie@libertytech.com>
2015-09-03 15:35:13 +0100

    fix one

--- public_html/Libs/database.php
+++ public_html/Libs/database.php
@@ -6,7 +6,7 @@ class database extends PDO {
 
 	function __construct(){
 
-		parent::__construct(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME, DB_USER ,DB_PASS);
+		parent::__construct(DB_TYPE.':host= '.DB_HOST.';dbname='.DB_NAME, DB_USER ,DB_PASS);
 
 	}
 
